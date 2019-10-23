688d4a7b9fd4ef94901ed9632f30a8109a95fd69
Perjevlig <per_haavard_1997@hotmail.com>
2019-10-23 20:25:55 +0200

    map beta

--- javascript/map.js
+++ javascript/map.js
@@ -1,14 +1,10 @@
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
+  var torg = {lat: 63.429994, lng: 10.393383};
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
+      document.getElementById('map'), {zoom: 15, center: torg});
   // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+  var marker = new google.maps.Marker({position: torg, map: map});
 }
-async defer {
-
-}
-    src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
