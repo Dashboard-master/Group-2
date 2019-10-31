e988b1e2de3912ff2ed852dff33d03fe3db92e30
Jonathan <jonnik1233@hotmail.com>
2019-10-31 16:00:25 +0100

    committer for å pulle main

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -8,38 +8,38 @@ function dailyMenu() {
 
     if (day === 0) {
         console.log("sunday")
-        var ImageSrc = "media/SøndagTest.jpg"
-        var ImageAlt = "Menu for sunday"
+        imageSrc = "media/SøndagTest.jpg"
+        imageAlt = "Menu for sunday"
     } else if (day === 1) {
         console.log("monday")
-        var ImageSrc = "media/MandagTest.jpg"
-        var ImageAlt = "Menu for monday"
+        imageSrc = "media/MandagTest.jpg"
+        imageAlt = "Menu for monday"
     } else if (day === 2) {
         console.log("tuesday")
-        var ImageSrc = "media/TirsdagTest.jpg"
-        var ImageAlt = "Menu for tuesday"
+        imageSrc = "media/TirsdagTest.jpg"
+        imageAlt = "Menu for tuesday"
     } else if (day === 3) {
         console.log("wednesday")
-        var ImageSrc = "media/OnsdagTest.jpg"
-        var ImageAlt = "Menu for wednesday"
+        imageSrc = "media/OnsdagTest.jpg"
+        imageAlt = "Menu for wednesday"
     } else if (day === 4) {
         console.log("thursday")
-        var ImageSrc = "media/TorsdagTest.jpg"
-        var ImageAlt = "Menu for thursday"
+        imageSrc = "media/TorsdagTest.jpg"
+        imageAlt = "Menu for thursday"
     } else if (day === 5) {
         console.log("friday")
-        var ImageSrc = "media/FredagTest.jpg"
-        var ImageAlt = "Menu for friday"
+        imageSrc = "media/FredagTest.jpg"
+        imageAlt = "Menu for friday"
     } else if (day === 6) {
         console.log("saturday")
-        var ImageSrc = "media/LørdagTest.jpg"
-        var ImageAlt = "Menu for saturday"
+        imageSrc = "media/LørdagTest.jpg"
+        imageAlt = "Menu for saturday"
     } else {
         console.log("Error finding day")
     }
 
-    document.getElementById("dailyImage").src = imageSrc
-    document.getElementById("dailyImage").alt = imageAlt
+    document.getElementById("dailyMenu").src = imageSrc
+    document.getElementById("dailyMenu").alt = imageAlt
 
 }
 
