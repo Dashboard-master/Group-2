fd423b37d3c31ea2d3e2a929fba913f82156a005
Perjevlig <per_haavard_1997@hotmail.com>
2019-11-01 13:24:45 +0100

    mye rart

--- javascript/map.js
+++ javascript/map.js
@@ -1,10 +1,10 @@
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
-  var torg = {lat: 63.429994, lng: 10.393383};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 15, center: torg});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: torg, map: map});
+  // The location of Torg
+  let torg = {lat: 63.429994, lng: 10.393383};
+  // The map, centered at Torg
+  let map = new google.maps.Map(
+      document.getElementById("map"), {zoom: 14.5, center: torg});
+  // The marker, positioned at Torg
+  let marker = new google.maps.Marker({position: torg, map: map});
 }
