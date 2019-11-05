8e695e00ce2257f7462b040a88fa1610cd90c82e
tpircs <aschjoth@gmail.com>
2019-11-05 15:19:16 +0100

    optimization of javascript

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -8,44 +8,42 @@ function dailyMenu() {
     var todayText = "Today: "
     var price = "Price"
 
+	days = ({"src": "media/test_image.jpg", "text": "Today: Monday", "price": "45,-"})
+	todayText = days[day]["text"]
+	imageSrc = days[day]["src"]
+	price = days[day]["price"]
+
     if (day === 0) {
-        console.log("sunday")
         imageSrc = "media/test_image.jpg"
         imageAlt = "Menu for sunday"
         todayText = "Today: Sunday"
         price = "45,-"
     } else if (day === 1) {
-        console.log("monday")
         imageSrc = "media/test_image_2.jpg"
         imageAlt = "Menu for monday"
         todayText = "Today: Monday"
         price = "35,-"
     } else if (day === 2) {
-        console.log("tuesday")
         imageSrc = "media/test_image_3.jpg"
         imageAlt = "Menu for tuesday"
         todayText = "Today: Tuesday"
         price = "39,-"
     } else if (day === 3) {
-        console.log("wednesday")
         imageSrc = "media/test_image.jpg"
         imageAlt = "Menu for wednesday"
         todayText = "Today: Wednesday"
         price = "45,-"
     } else if (day === 4) {
-        console.log("thursday")
         imageSrc = "media/test_image_3.jpg"
         imageAlt = "Menu for thursday"
         todayText = "Today: Thursday"
         price = "39,-"
     } else if (day === 5) {
-        console.log("friday")
         imageSrc = "media/test_image.jpg"
         imageAlt = "Menu for friday"
         todayText = "Today: Friday"
         price = "45,-"
     } else if (day === 6) {
-        console.log("saturday")
         imageSrc = "media/test_image_2.jpg"
         imageAlt = "Menu for saturday"
         todayText = "Today: Saturday"
