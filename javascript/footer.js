eb66c16b8b5f5a44cb397df8e0902eab0c5c0f22
VigdisIS <vigdis.steinsund@hotmail.com>
2019-11-07 11:33:43 +0100

    small fix responsive + footer mobile ver fix

--- javascript/footer.js
+++ javascript/footer.js
@@ -1,6 +1,7 @@
 let footer = document.createElement("FOOTER");
 footer.classList.add("footer");
 let footer_table = document.createElement("TABLE");
+footer_table.id = "footer_table";
 let footer_row = document.createElement("TR");
 let footer_column1 = document.createElement("TD");
 let footer_column2 = document.createElement("TD");
@@ -10,9 +11,6 @@ footer_column1.style = "border-right: 2px solid #FFFAF3; padding-right: 15px;";
 
 footer_column2.innerText = "Tinghusplassen 24, 7013 Trondheim";
 footer_column2.style = "border-left: 2px solid #FFFAF3; padding-left: 15px;";
-
-footer_table.style =
-  "border-collapse: collapse; max-width: max-content; margin:auto; color: #FFFAF3;";
 
 footer_row.appendChild(footer_column1);
 footer_row.appendChild(footer_column2);
