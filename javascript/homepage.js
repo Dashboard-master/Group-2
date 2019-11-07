9914cfb2744f6ee4c00329876c0c5973ed5c0b32
Jonathan <jonnik1233@hotmail.com>
2019-11-07 14:04:56 +0100

    pull

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,49 +1,54 @@
 function dailyMenu() {
+    // Getting the current date when the website is accessed.
     var todaysDate = new Date();
     var day = todaysDate.getDay();
     console.log(day);
 
+    // An array containing a daily text for the header element, a price for the daily menu and an image 
+    // and an alt attribute to that image to show in the "Daily menu" box on the homepage.
     days = [{
         todayText: "Sunday",
         price: "45,-",
-        imagesrc: "media/test_image.jpg",
+        imageSrc: "media/.jpg",
         imageAlt: "Menu for Sunday"
     }, {
         todayText: "Monday",
         price: "35,-",
-        imagesrc: "media/test_image_2.jpg",
+        imageSrc: "media/coffee_2.jpg",
         imageAlt: "Menu for Monday"
     }, {
         todayText: "Tuesday",
         price: "39,-",
-        imagesrc: "media/test_image_3.jpg",
+        imageSrc: "media/coffee.jpg",
         imageAlt: "Menu for Tuesday"
     }, {
         todayText: "Wednesday",
         price: "45,-",
-        imagesrc: "media/test_image.jpg",
+        imageSrc: "media/coffee_3.jpg",
         imageAlt: "Menu for Wednesday"
     }, {
         todayText: "Thursday",
         price: "39,-",
-        imagesrc: "media/test_image_3.jpg",
+        imageSrc: "media/coffee_2.jpg",
         imageAlt: "Menu for Thursday"
     }, {
         todayText: "Friday",
         price: "45,-",
-        imagesrc: "media/test_image.jpg",
+        imageSrc: "media/coffee.jpg",
         imageAlt: "Menu for Friday"
     }, {
         todayText: "Saturday",
         price: "35,-",
-        imagesrc: "media/test_image_2.jpg",
+        imageSrc: "media/coffee_3.jpg",
         imageAlt: "Menu for Saturday"
     }]
 
-    // document.getElementById("dailyMenu").src = days[day].imageSrc
+    // This code gives the id's in homepage.html the correct values for the corresponding day.
+    document.getElementById("dailyMenu").src = days[day].imageSrc
     document.getElementById("dailyMenu").alt = days[day].imageAlt
     document.getElementById("todayText").textContent = days[day].todayText
     document.getElementById("price").textContent = days[day].price
 }
 
+// The function to place the daily elements on the homepage is calles when the site is accessed.
 dailyMenu()
